ed8a49d9ca4e09451e6836aaf35f5fe38071e24a
angelawieber <tuf92449@temple.edu>
2018-04-25 10:57:54 -0400

    ints

--- chr21.py
+++ chr21.py
@@ -1,36 +1,102 @@
 from pysam import VariantFile
-vcf_in = VariantFile("/mnt/sda/data/1KG/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz")
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description = 'Find IBD modes for 2 individuals.')
+parser.add_argument("-v1", action = "store", dest = "vcf1")
+parser.add_argument("-v2", dest = "vcf2")
+parser.add_argument("-v3", dest = "vcf3")
+parser.add_argument("-v4", dest = "vcf4")
+parser.add_argument("-v5", dest = "vcf5")
+parser.add_argument("-v6", dest = "vcf6")
+parser.add_argument("-v7", dest = "vcf7")
+parser.add_argument("-v8", dest = "vcf8")
+parser.add_argument("-v9", dest = "vcf9")
+parser.add_argument("-v10", dest = "vcf10")
+parser.add_argument("-v11", dest = "vcf11")
+parser.add_argument("-v12", dest = "vcf12")
+parser.add_argument("-v13", dest = "vcf13")
+parser.add_argument("-v14", dest = "vcf14")
+parser.add_argument("-v15", dest = "vcf15")
+parser.add_argument("-v16", dest = "vcf16")
+parser.add_argument("-v17", dest = "vcf17")
+parser.add_argument("-v18", dest = "vcf18")
+parser.add_argument("-v19", dest = "vcf19")
+parser.add_argument("-v20", dest = "vcf20")
+parser.add_argument("-v21", dest = "vcf21")
+parser.add_argument("-v22", dest = "vcf22")
+parser.add_argument('-o', action = "store", dest = "output_filename")
+parser.add_argument('-p1', action = "store", dest = "pop")
+#parser.add_argument('-p2', action = "store", dest = "pop2")
+parser.add_argument('-i', action = "store", dest = "ind1")
+parser.add_argument('-j', action = "store", dest = "ind2")
+args = parser.parse_args()
+vcf_filename = [args.vcf1, args.vcf2, args.vcf3, args.vcf4, args.vcf5, args.vcf6, args.vcf7, args.vcf8, args.vcf9, args.vcf10, args.vcf11, args.vcf12, args.vcf13, args.vcf14, args.vcf15, args.vcf16, args.vcf17, args.vcf18, args.vcf19, args.vcf20, args.vcf21, args.vcf22]
+output_filename = args.output_filename
+pop = args.pop
+#pop2 = args.pop2
+ind1 = args.ind1
+ind2 = args.ind2
+#print args
+
+
+f = open("/mnt/sda/data/1KG/samples_and_pops.txt")
 counter = 0
-outfile = open("EUR_AF.txt","w")
-for rec in vcf_in.fetch():
+outfile = open(output_filename,"a")	
+
+IDS = []
+for line in f:
+	sub_pop = line.split()[1] 
+	if sub_pop == pop: IDS.append(line.split()[0])
+print IDS
+for vcf in vcf_filename: 
+	vcf_in = VariantFile(vcf)
+	for rec in vcf_in.fetch():
 	#only print biallelic sites (i.e. ones with only 2 alleles)
 	#print the DERIVED allele frequency, i.e. the frequency of the non-ancestral allele
 	#skp ones where there's no ancestral allele called
-	counter += 1 
-	if counter > 2000: break
-	if "AA" not in rec.info: continue
-	if len(rec.alts) != 1 or len(rec.ref) != 1: continue
+		counter += 1 
+	#if counter > 2000: break
+		if "AA" not in rec.info: continue
+		if len(rec.alts) != 1 or len(rec.ref) != 1: continue
 	#print rec.chrom, rec.ref, rec.alts[0], rec.pos, rec.info["EUR_AF"][0]
-	AAfield = rec.info["AA"]
-	AAsplit = AAfield.split("|")
-	AA = AAsplit[0].upper() #TODO: figure out what lower case means
-	if AA == "." or AA == "": continue
-	alt_freq = rec.info["EUR_AF"][0] #+ rec.info["AFR_AF"][0] + rec.info["AMR_AF"][0] + rec.info["EAS_AF"][0] + rec.info["SAS_AF"][0] 
-	if rec.ref != AA: alt_freq = 1 - alt_freq
-	if alt_freq == 0 or alt_freq ==1: continue
+		AAfield = rec.info["AA"]
+		AAsplit = AAfield.split("|")
+		AA = AAsplit[0].upper() 
+		if (AA != 'A' and AA != 'C' and AA != 'T' and AA != 'G'): continue
+		alt_freqs = []
+		num_alt_alleles = 0.0 #keeps track of how many alternative alleles you've seen so far for this site
+		num_good_alleles = 0.0 #keeps track of how man non-missing alleles you've seen so far
+		num_none = 0
+#		alt_freq = float(rec.info["EUR_AF"][0])
+		for ID in IDS:
+			genotype = rec.samples[ID].allele_indices
+			num_alt_alleles += sum(genotype)
+			for geno in genotype:
+				 if geno == None: num_none + 1
+			num_good_alleles += len(genotype) - num_none
+	#count the alleles that aren't None
+		freq = num_alt_alleles/num_good_alleles
+	#from here it's like you just got he EUR_AF
+		if rec.ref != AA: freq = 1 - freq
+		alt_freqs.append(freq)
+	#print alt_freqs 
+		alt_freq = sum(alt_freqs)/len(alt_freqs)
+		if alt_freq == 0 or alt_freq == 1: continue
 	#get what the derived allele is
-	if AA == rec.ref: der = rec.alts[0]
-	elif AA == rec.alts[0]: der = rec.ref
-	switch = {(0,0):(1,1), (0,1):(1,0), (1,0):(0,1), (1,1):(0,0)} 
-	GT1 = rec.samples["HG00096"].allele_indices
-	if AA == rec.alts[0]: GT1 = switch[GT1]
-	GT2 = rec.samples["HG00097"].allele_indices
-	if AA == rec.alts[0]: GT2 = switch[GT2]
-	config = GT1 + GT2
-	config_dict = {(0,0,0,0):0, (0,0,1,1):1, (0,0,0,1):2, (0,0,1,0):2, (0,1,0,0):3, (1,0,0,0):3, (0,1,0,1):4, (1,0,0,1):4, (1,0,1,0):4, (0,1,1,0):4,
- 	(1,0,1,1):5, (0,1,1,1):5, (1,1,0,1):6, (1,1,1,0):6, (1,1,0,0):7, (1,1,1,1):8} 	
-	config = config_dict[config]
+		if AA == rec.ref: der = rec.alts[0]
+		if len(der) != 1: continue 
+		elif AA == rec.alts[0]: der = rec.ref
+		switch = {(0,0):(1,1), (0,1):(1,0), (1,0):(0,1), (1,1):(0,0)} 
+		GT1 = rec.samples[ind1].allele_indices
+		if AA == rec.alts[0]: GT1 = switch[GT1]
+		GT2 = rec.samples[ind2].allele_indices
+		if AA == rec.alts[0]: GT2 = switch[GT2]
+		config = GT1 + GT2
+		config_dict = {(0,0,0,0):0, (0,0,1,1):1, (0,0,0,1):2, (0,0,1,0):2, (0,1,0,0):3, (1,0,0,0):3, (0,1,0,1):4, (1,0,0,1):4, (1,0,1,0):4, (0,1,1,0):4,
+	 	(1,0,1,1):5, (0,1,1,1):5, (1,1,0,1):6, (1,1,1,0):6, (1,1,0,0):7, (1,1,1,1):8} 	
+		config = config_dict[config]
 	#map this to the genotype configurations numbers
 	#add that as another column to the output.
-	outfile.write("%s\t%d\t%s\t%s\t%f\t%s\n"%(rec.chrom,rec.pos,AA,der,alt_freq,config))
+		outfile.write("%s\t%d\t%s\t%s\t%f\t%d\n"%(rec.chrom,rec.pos,AA,der,alt_freq,config))
 
--- mulligan.py
+++ mulligan.py
@@ -1,49 +1,177 @@
 import numpy as np
+import random
+from numpy.random import uniform
 import math
+from scipy import optimize
+from scipy.special import comb 
+from scipy.stats import binom 
+from scipy.stats import uniform
+import sys
+import matplotlib
+import cPickle #this is to save stuff
+import argparse
+import warnings
+import scipy
+from scipy.sparse.linalg import expm_multiply
 
 #deltas are an array [delta1, delta2, delta3, ..., delta9] which are the parameters of the distribution
-#genotype data should be a list of numbers 0 through 4 corresponding to the appropriate genotype configurations. Each entry is a SINGLE SITE
-#allele_frequencies is an array of the frequencies of one of the alleles at every site
-genotype_data = [0, 1, 2, 3, 4]
-def inbreeding_likelihood(delta, genotype_data, allele_frequencies):
-	if len(genotype_data) != len(allele_frequencies):
-		print "ERROR: genotype data and allele frequencies not the same length"
-		return None
-	likelihood = 0
-	for i in range(len(genotype_data)): 
-		cur_likelihood = np.log(inbreeding_likelihood_one_site(delta, genotype_data[i], allele_frequencies[i]))
-		print cur_likelihood
-		likelihood += cur_likelihood
-	return likelihood
+#parser = argparse.ArgumentParser()
+#parser.add_argument("-f", dest = "filename")
+#parser.add_argument("-d", type = float, dest = "distance")
+#args = parser.parse_args()
+#filename = args.filename
+#distance = args.distance
 
-def populate_prob_table(allele_freq):
-	table = np.zeros((9,9))
-	p = allele_freq
-	print p
-	table[0] = (1-p, (1-p)**2, (1-p)**2, (1-p)**3, (1-p)**2, (1-p)**3, (1-p)**2, (1-p)**3, (1-p)**4)
-	table[1] = (0, p*(1-p), 0, (1-p)*(p**2), 0, p*(1-p)**2, 0, 0, (p**2)*(1-p)**2)
-	table[2] = (0, 0, p*(1-p), 2*p*(1-p)**2, 0, 0, 0, p*(1-p)**2, 2*p*(1-p)**3)
-	table[3] = (0, 0, 0, 0, p*(1-p), 2*p*(1-p)**2, 0, p*(1-p)**2, 2*p*(1-p)**3)
- 	table[4] = (0, 0, 0, 0, 0, 0, 2*p*(1-p), p*(1-p), 4*(p**2)*(1-p)**2)
-	table[5] = (0, 0, 0, 0, p*(1-p), 2*(p**2)*(1-p), 0, (p**2)*(1-p), 2*(p**3)*(1-p))
-	table[6] = (0, 0, p*(1-p), 2*(p**2)*(1-p), 0, 0, 0, (p**2)*(1-p), 2*(p**3)*(1-p))
-	table[7] = (0, p*(1-p), 0, p*(1-p)**2, 0, (p**2)*(1-p), 0, 0, (p**2)*(1-p)**2)
-	table[8] = (p, p**2, p**2, p**3, p**2, p**3, p**2, p**3, p**4)
+def find_p(n, x, t1, t2):
+	h = []
+	for i in list(range(n+1)):
+		h.append((x**(i))*((1-x)**(n-i))) 
+	h = np.asarray(h)
+	Q_t2 = np.zeros((n+1, n+1))
+	for i in range(n+1): 
+		Q_t2[i][i] = -i*(n-i)
+		if i >= 1: Q_t2[i][i-1] = 0.5*i*(i-1)
+		if i < n: Q_t2[i][i+1] = 0.5*(n-i)*(n-i-1)
+	Q_t2 = Q_t2 * t2 
 
+	Q_t1 = np.zeros((n+1, n+1))
+	for i in range(n+1): 
+		Q_t1[i][i] = -i*(n-i+1)
+		if i < n: Q_t1[i][i+1] = 0.5*(n-i+1)*(n-i)
+		if i >= 1: Q_t1[i][i-1] = 0.5*i*(i-1)
+	Q_t1 = Q_t1 * t1
+
+	Q_t1h = scipy.sparse.linalg.expm_multiply(Q_t1, h)
+	p = scipy.sparse.linalg.expm_multiply(Q_t2, Q_t1h) 
+		
+	return p 
+
+def populate_prob_table(allele_frequencies, t1, t2):
+	table = np.zeros((len(allele_frequencies),9,9))
+	for i in range(len(allele_frequencies)): 
+		x = allele_frequencies[i]
+		n1 = find_p(1, x, t1, t2)
+		n2 = find_p(2, x, t1, t2)
+		n3 = find_p(3, x, t1, t2)
+		n4 = find_p(4, x, t1, t2)
+
+		table[i,0] = (n1[0], n2[0], n2[0], n3[0], n2[0], n3[0], n2[0], n3[0], n4[0])
+		table[i,1] = (0, n2[1], 0, n3[2], 0, n3[1], 0, 0, n4[2])
+		table[i,2] = (0, 0, n2[1], 2*n3[1], 0, 0, 0, n3[1], 2*n4[1])
+		table[i,3] = (0, 0, 0, 0, n2[1], 2*n3[1], 0, n3[1], 2*n4[1])
+		table[i,4] = (0, 0, 0, 0, 0, 0, 2*n2[1], n3[2] + n3[1], 4*n4[2])
+		table[i,5] = (0, 0, 0, 0, n2[1], 2*n3[1], 0, n3[2], 2*n4[3])
+		table[i,6] = (0, 0, n2[1], 2*n3[2], 0, 0, 0, n3[2], 2*n4[3])
+		table[i,7] = (0, n2[1], 0, n3[1], 0, n3[2], 0, 0, n4[2])
+		table[i,8] = (n1[1], n2[2], n2[2], n3[3], n2[2], n3[3], n2[2], n3[3], n4[4])
+	#rows are IBS, columns are IBD
+#		print table[i]
+#		raw_input()
 	return table
-
-
-#delta is still all the deltas
-#genotype is just ONE genotype, for one site
-#allele_freq is just ONE allel frequency 
-def inbreeding_likelihood_one_site(delta, genotype, allele_freq):
-	prob_table = populate_prob_table(allele_freq)
-	likelihood = np.dot(prob_table[genotype], delta)
-	return likelihood
-
-test_like = inbreeding_likelihood([1/9.]*9,[3,1,3,2,0,4],[.5,.1,.3,.7,.8,.2])
-print test_like
-
-
-
 	
+def create_data_given_IBS(N_1, K_1, N_2, K_2, err1, err2, allele_frequencies):
+	data_given_IBS = []
+	for i in range(len(allele_frequencies)):
+		num_of_sites = len(N_1[i]) #( == len(K_1[i]) == lenN_2[i]) = len(K_2[i]) 
+	#	print "num of sites =", num_of_sites	
+		data_given_IBS.append(np.zeros((num_of_sites,9)))
+	#	print "N1 length =", len(N_1[i]), "K1 length =", len(K_1[i])
+		pmf_K1_N1_err = binom.pmf(K_1[i], N_1[i], err1)
+		pmf_K2_N2_err = binom.pmf(K_2[i], N_2[i], err2)
+		pmf_K1_N1_05 = binom.pmf(K_1[i], N_1[i], 0.5)
+		pmf_K2_N2_05 = binom.pmf(K_2[i], N_2[i], 0.5)
+		pmf_K1_N1_minus_err = binom.pmf(K_1[i], N_1[i], 1-err1)
+		pmf_K2_N2_minus_err = binom.pmf(K_2[i], N_2[i], 1-err2)
+		data_given_IBS[i] = np.transpose((pmf_K1_N1_err * pmf_K2_N2_err,
+				pmf_K1_N1_err * pmf_K2_N2_minus_err,
+				pmf_K1_N1_err * pmf_K2_N2_05,
+				pmf_K1_N1_05 * pmf_K2_N2_err,
+				pmf_K1_N1_05 * pmf_K2_N2_05,
+				pmf_K1_N1_05 * pmf_K2_N2_minus_err,
+				pmf_K1_N1_minus_err * pmf_K2_N2_05,
+				pmf_K1_N1_minus_err * pmf_K2_N2_err,
+				pmf_K1_N1_minus_err * pmf_K2_N2_minus_err)) 
+#		print "data_given_IBS[i]", data_given_IBS[i][0]
+#		raw_input()
+	return data_given_IBS 
+	
+def inbreeding_likelihood(N_1, K_1, N_2, K_2, err1, err2, t1, t2, allele_frequencies, delta):
+#	should be the modern allele frequency
+	delta_sum = sum(delta)
+	if delta_sum > 1: 
+		likelihood = -1e300
+		return likelihood
+	delta = np.append(delta,(1-delta_sum))
+	IBS_given_IBD = populate_prob_table(allele_frequencies, t1, t2)
+	data_given_IBS = create_data_given_IBS(N_1, K_1, N_2, K_2, err1, err2, allele_frequencies)
+#	print data_given_IBS[0][0]
+	#calculating prob_IBS and prob_data should happen in the loop over allele frequcies
+#	cur_likelihood = 0.
+	loglikelihood = 0.
+	cur_likelihood = []
+	for i in range(len(allele_frequencies)):
+		if allele_frequencies[i] == 0 or allele_frequencies[i] == 1: continue
+#		with warnings.catch_warnings(record = True):
+#			warnings.filterwarnings('error')
+#			warnings.simplefilter('default')
+#			try:
+		prob_IBS = np.dot(IBS_given_IBD[i], delta)
+#				print "len prob_IBS=", len(prob_IBS), "len data_given_IBS[i] =", len(data_given_IBS[i])
+		prob_data = np.dot(data_given_IBS[i], prob_IBS)
+#				print data_given_IBS[i], " ---\n", prob_IBS
+#				raw_input()
+		cur_likelihood = np.log(prob_data)
+#				warnings.warn(Warning())
+#		print cur_likelihood
+#			except Warning:
+		which_zero = np.where(prob_data == 0)[0]
+		if len(which_zero) >= 1:
+			which_zero = which_zero[:10]
+#			print "PROB IBS GIVEN IBD"
+#			print IBS_given_IBD[i]
+#			print "PROB IBS"
+#			print prob_IBS 
+#			print "NUMBER OF ZEROS AND NUMBER OF DATA"
+#			print len(which_zero), len(prob_data)
+#			print "DATA"
+#			print which_zero, N_1[i][which_zero], K_1[i][which_zero], N_2[i][which_zero], K_2[i][which_zero]
+#			print "PROB DATA GIVEN IBS"
+#			print data_given_IBS[i][which_zero]
+#			print "PROB DATA"
+#			print prob_data[which_zero]
+#			print allele_frequencies[i]
+#			raw_input()
+#				print "Dot product of 0", delta 
+#				return -np.inf
+#			finally:
+		loglikelihood += np.sum(cur_likelihood)
+#	print delta, t1, t2, err1, err2, loglikelihood
+	return loglikelihood
+
+#TO SAVE INTO A PICKLE
+#pickle_file = open("LnL.pickle","w")
+#cPickle.dump(LnL,pickle_file)
+#pickle_file.close()
+
+#TO LOAD
+#pickle_file = open("LnL.pickle")
+#LnL = cPickle.load(pickle_file)
+#pickle_file.close()
+
+
+
+def main(delta7, delta8, N_1, K_1, N_2, K_2, allele_frequencies):
+
+	t = [uniform.rvs(loc = 1e-7, scale = 2, size = 2)]
+	e = [uniform.rvs(loc = 1e-6, scale = 0.5, size = 2)]
+	x0 = np.append(t, e)
+
+#pars = [tl, t2, err1, err2]
+
+#	opt = optimize.fmin_l_bfgs_b(func = lambda pars: -inbreeding_likelihood(N_1, K_1, N_2, K_2, pars[2], pars[3], pars[0], pars[1], allele_frequencies, [0,0,0,0,0,0,delta7,delta8]), x0 = x0, approx_grad = True, epsilon = 1e-5, bounds =  [[1e-6,2]]*2 + [[1e-6,.5]]*2, factr = 1, pgtol = 1e-12)	 
+	opt = optimize.fmin_l_bfgs_b(func = lambda pars: -inbreeding_likelihood(N_1, K_1, N_2, K_2, pars[0], pars[1], 0, 0, allele_frequencies, [0,0,0,0,0,0,delta7,delta8]), x0 = x0, approx_grad = True, epsilon = 1e-5, bounds =  [[1e-6,2]]*2 + [[1e-6,.5]]*2, factr = 1, pgtol = 1e-12)	 
+#opt = optimize.fmin_tnc(func = lambda pars: -inbreeding_likelihood(N_1, K_1, N_2,K_2, pars[4], pars[5], pars[2], pars[3], allele_frequencies, [0, 0, 0, 0, 0, 0, pars[0], pars[1]]), x0 = x0, approx_grad = True, bounds = ([[1e-6,1]]*2 + [[1e-6,2]]*2 + [[1e-6,0.5]]*2),  epsilon = 1e-8, maxfun = 1000)
+#	opt = optimize.fmin_cobyla(func = lambda pars: -inbreeding_likelihood(N_1, K_1, N_2, K_2, pars[4], pars[5], pars[2], pars[3], allele_frequencies, [0,0,0,0,0,0,pars[0],pars[1]]), x0 = x0, cons = [constr1, constr2, constr3, constr4, constr5, constr6, constr7, constr8, constr9], rhobeg = 0.05, maxfun=10000) 
+ 
+	print opt
+	return opt
